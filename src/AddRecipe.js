f7122d57c6d5e7aad3229de04916bf6c7b8316d3
jonathan bola <bolajonathan@yahoo.com>
2022-03-28 16:45:01 -0500

    all-in-one

--- src/AddRecipe.js
+++ src/AddRecipe.js
@@ -2,12 +2,35 @@ import React from 'react'
 import './Addrecipe.css'
 
 export default function Addrecipe() {
+  let formData = {
+    name: "",
+    image:"",
+    steps:""}
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+      formData.name= e.target.recipeName.value
+      formData.image = e.target.imageUrl.value
+      formData.steps= e.target.steps.value
+
+      fetch("http://localhost:3000/recipes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"},
+      body: JSON.stringify(formData)
+    })
+    
+    
+    }
+
   return <>
     <div className="addrecipe-container">
-  <form>
+  <form id='addRecipe-form' onSubmit={handleSubmit}>
 
     <label for="fname">Recipe name</label>
-    <input type="text" id="RecipeName" name="firstname" placeholder="Recipe name.."/>
+    <input type="text" id="recipeName" name="firstname" placeholder="Recipe name.."/>
 
     <label for="lname">Image URL</label>
     <input type="text" id="imageUrl" name="lastname" placeholder="Image URL here"/>
--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import { Route, Switch } from "react-router-dom";
 
 
 
+
+
 function App() {
   return (
     <div className="App">
